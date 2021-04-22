a64a96aafe15238e9d41fe7c63015d8a56557364
david111esc <david111esc@gmail.com>
2021-04-22 22:13:19 +0800

    feat: add graph and naive train perceptron

--- perceptron-family/src/naive_perceptron.rs
+++ perceptron-family/src/naive_perceptron.rs
@@ -1,16 +1,43 @@
 use itertools::izip;
 use ndarray::prelude::*;
+extern crate plotly;
+use plotly::common::Mode;
+use plotly::{ImageFormat, Plot, Scatter};
+
 fn quick_start() {
     let d = 2;
-    let mut w = Array::<f64, _>::zeros((1, d));
-    let mut b: f64 = 0_f64;
+    let mut w = Array::<f64, Ix1>::zeros(d);
+    let mut b = Array::<f64, Ix1>::zeros(1);
     println!("w0: {}", w);
-    let x = array![[1., 0.], [1., 1.]];
-    let y = array![[1.], [-1.]];
-    for (data, target) in izip!(x.rows(), y.rows()) {
-        let g = &data * &target;
-        println!("data={}, target={},g={}", data, target, g);
+
+    let x = array![[1., 0.], [0.4, 0.], [1.4, 0.], [1., 1.]];
+    let y = array![[1.], [1.], [1.], [-1.]];
+    let trace1 = Scatter::new(x.clone().column(0).to_vec(), x.clone().column(1).to_vec())
+        .mode(Mode::Markers);
+    let mut plot = Plot::new();
+    plot.add_trace(trace1);
+    plot.show();
+    let mut times = 0;
+    loop {
+        let mut flag: bool = true;
+        for (data, target) in izip!(x.rows(), y.rows()) {
+            let obj = &target[0] * (&w.dot(&data) + &b[0]);
+            println!("obj={}", obj);
+            if obj <= 0. {
+                times = times + 1;
+                flag = false;
+                let g = &data * &target;
+                w = w + &g;
+                b = b + target[0];
+                println!("data={}, target={},g={}", data, target, g);
+                println!("w={}, b={}", w, b);
+            }
+        }
+        if flag {
+            break;
+        }
     }
+    println!("Total update times: {}", times);
 }
 
 trait NaivePerceptron {
